4460ec7d4598f4539be3e313c5a0cddf99fc3f33
Allaaxx <allanrao358@gmail.com>
2026-07-25 17:15:47 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,22 +1,56 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import LoaderIcon from "../assets/icons/loader.svg?react"
+import DetailsIcon from "../assets/icons/details.svg?react"
+
 const TaskItem = ({ task }) => {
-  const getStatusClasses = () => {
+  const getContainerClasses = () => {
     if (task.status === "done") {
       return "bg-pink-500/10 text-pink-500"
     }
-
     if (task.status === "in_progress") {
       return "bg-[#FFAA04]/10 text-[#FFAA04]"
     }
-
-    if (task.status === "not_started") {
-      return "bg-[#35383E]/10 text-[#35383E]"
-    }
+    return "bg-[#35383E]/10 text-[#35383E]"
   }
+
+  const getCheckboxClasses = () => {
+    if (task.status === "done") {
+      return "bg-pink-500 text-white"
+    }
+    if (task.status === "in_progress") {
+      return "bg-[#FFAA04] text-white"
+    }
+    return "bg-[#35383E]/5 text-[#35383E]"
+  }
+
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg px-4 py-3 text-sm transition ${getContainerClasses()}`}
     >
-      {task.title}
+      <div className="flex w-full items-center justify-between gap-2">
+        <div className="flex items-center gap-2">
+          <label
+            className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg transition-colors ${getCheckboxClasses()}`}
+          >
+            <input
+              type="checkbox"
+              checked={task.status === "done"}
+              className="absolute h-full w-full cursor-pointer opacity-0"
+              readOnly
+            />
+            {task.status === "done" && <CheckIcon className="h-4 w-4" />}
+            {task.status === "in_progress" && (
+              <LoaderIcon className="h-4 w-4 animate-spin" />
+            )}
+          </label>
+
+          {task.title}
+        </div>
+
+        <a href="#" className="hover: opacity-75 transition-colors">
+          <DetailsIcon />
+        </a>
+      </div>
     </div>
   )
 }
