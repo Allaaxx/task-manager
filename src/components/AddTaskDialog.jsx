3b912718757d80972d7b5c69496d485bdcee70fb
Allaaxx <allanrao358@gmail.com>
2026-08-03 11:47:30 -0300

    feat: ensure AddTaskDialog can be closed

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,14 +1,39 @@
 import { createPortal } from "react-dom"
 
-const AddTaskDialog = ({ isOpen }) => {
+import Button from "./Button"
+import Input from "./Input"
+
+const AddTaskDialog = ({ isOpen, handleClose }) => {
   if (!isOpen) return null
   return createPortal(
     <div className="fixed top-0 bottom-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur">
       <div className="rounded-xl bg-white p-5 text-center shadow">
         <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mt-1 text-sm text-[#9A9C9F]">
+        <p className="mt-1 mb-4 text-sm text-[#9A9C9F]">
           insira as informações abaixo
         </p>
+        <div className="flex w-84 flex-col space-y-4">
+          <Input id="title" label="Título" placeholder="Name" />
+          <Input id="time" label="Horário" placeholder="Horário" />
+          <Input
+            id="description"
+            label="Descrição"
+            placeholder="Descreva a Tarefa"
+          />
+          <div className="flex gap-3">
+            <Button
+              size="large"
+              variant="secondary"
+              className="w-full"
+              onClick={handleClose}
+            >
+              Cancelar
+            </Button>
+            <Button size="large" className="w-full">
+              Salvar
+            </Button>
+          </div>
+        </div>
       </div>
     </div>,
     document.body
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,19 +2,24 @@ import { useState } from "react"
 import { toast } from "sonner"
 
 import { CloudSunIcon, MoonIcon, SunIcon } from "../assets/icons"
+import { AddIcon, TrashIcon } from "../assets/icons"
 import TASKS from "../constants/tasks"
-import TaskHeader from "./TaskHeader"
+import AddTaskDialog from "./AddTaskDialog"
+import Button from "./Button"
 import TaskItem from "./TaskItem"
 import TasksSeparator from "./TasksSeparator"
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS)
+  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
 
   const morningTasks = tasks.filter((task) => task.time === "morning")
-
   const afternoonTasks = tasks.filter((task) => task.time === "afternoon")
-
   const eveningTasks = tasks.filter((task) => task.time === "evening")
+
+  const handleDialogClose = () => {
+    setAddTaskDialogIsOpen(false)
+  }
 
   const handleTaskCheckboxClick = (taskId) => {
     const newTasks = tasks.map((task) => {
@@ -51,7 +56,30 @@ const Tasks = () => {
 
   return (
     <div className="w-full space-y-6 px-8 py-16">
-      <TaskHeader />
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text text-xs font-semibold text-pink-500">
+            Minhas Tarefas
+          </span>
+          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant="ghost">
+            <TrashIcon />
+            Limpar tarefas
+          </Button>
+
+          <Button onClick={() => setAddTaskDialogIsOpen(true)}>
+            <AddIcon />
+            Nova Tarefa
+          </Button>
+          <AddTaskDialog
+            isOpen={addTaskDialogIsOpen}
+            handleClose={handleDialogClose}
+          />
+        </div>
+      </div>
 
       <div className="rounded-xl bg-white p-6">
         <div className="space-y-3">
