7684f9b67229e7733f257a8d9300f9daa05219e8
Allaaxx <allanrao358@gmail.com>
2026-08-12 10:20:05 -0300

    chore: add prop-types to Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types"
+
 import InputErrorMessage from "./InputErrorMessage"
 import InputLabel from "./InputLabel"
 
@@ -13,6 +15,13 @@ const Input = ({ label, errorMessage, ...rest }) => {
       {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
     </div>
   )
+}
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
+  placeholder: PropTypes.string,
 }
 
 export default Input
