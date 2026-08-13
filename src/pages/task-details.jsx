b9a5021d096bf27b2038db2bd0078cd97cbdd33d
Allaaxx <allanrao358@gmail.com>
2026-08-13 09:16:04 -0300

    feat: add initial TaskDetails page without logic

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
+
+import { ArrowLeftIcon, ChevronRightIcon, TrashIcon } from "../assets/icons"
+import Button from "../components/Button"
+import Input from "../components/Input"
+import Sidebar from "../components/Sidebar"
+import TimeSelect from "../components/TimeSelect"
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState()
+  const navigate = useNavigate()
+  const handleBackClick = () => {
+    navigate(-1)
+  }
+
   useEffect(() => {
     const fetchTasks = async () => {
       const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
@@ -14,10 +25,68 @@ const TaskDetailsPage = () => {
     }
     fetchTasks()
   }, [taskId])
+
   return (
-    <div>
-      <h1>{task?.title}</h1>
-      <h1>{task?.description}</h1>
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full space-y-6 px-8 py-16">
+        {/* barra do top */}
+        <div className="flex w-full justify-between">
+          {/* parte da esquerda */}
+          <div>
+            <button
+              className="mb-3 flex h-8 w-8 items-center justify-center rounded-full bg-brand-magenta"
+              onClick={handleBackClick}
+            >
+              <ArrowLeftIcon />
+            </button>
+            <div className="flex items-center gap-1 text-xs">
+              <span
+                onClick={handleBackClick}
+                className="cursor-pointer text-brand-text-gray"
+              >
+                Minhas Tarefas
+              </span>
+              <ChevronRightIcon className="text-brand-text-gray" />
+              <span className="font-semibold text-brand-magenta">
+                {task?.title}
+              </span>
+            </div>
+
+            <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
+          </div>
+          {/* parte da direita */}
+          <Button className="h-fit self-end" color="danger">
+            <TrashIcon />
+            Deletar tarefa
+          </Button>
+        </div>
+        {/* dados da tarefa */}
+        <div className="space-y-6 rounded-xl bg-white p-6">
+          <div>
+            <Input id="title" label="Titúlo" value={task?.title} />
+          </div>
+          <div>
+            <TimeSelect value={task?.time} />
+          </div>
+          <div>
+            <Input
+              id="description"
+              label="Descrição"
+              value={task?.description}
+            />
+          </div>
+        </div>
+        {/* botões */}
+        <div className="flex w-full justify-end gap-3">
+          <Button size="large" color="secondary">
+            Cancelar
+          </Button>
+          <Button size="large" color="primary">
+            Salvar
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
